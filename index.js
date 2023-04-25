f231fa317c67b182d8c4b92b6bb0b45e4f0e5544
Alexis <35.alexis.pele@gmail.com>
2023-04-25 18:50:13 +0200

    On second thought express can do the job

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+const fs = require('fs');
+const path = require('path');
+const GeoJsonGeometriesLookup = require("geojson-geometries-lookup");
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', (req, res) => {
+    const lat = parseFloat(req.query.lat);
+    const lon = parseFloat(req.query.lon);
+
+    if (!lat || !lon) return
+
+    const f = fs.readFileSync(path.join(__dirname, 'public/circonscriptions-legislatives.json'), 'utf-8');
+    const data = JSON.parse(f)
+    
+    gl = new GeoJsonGeometriesLookup(data);
+
+    const pos = {
+        type: "Point",
+        coordinates: [lon, lat],
+    };
+
+    res.json({
+        "code": gl.getContainers(pos).features[0]?.properties["REF"]
+    })
+})
+
+app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
