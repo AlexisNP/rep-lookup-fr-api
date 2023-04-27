44f59a3eb19237ba325769a82a27b3ce0e564fe8
Alexis <35.alexis.pele@gmail.com>
2023-04-27 12:11:53 +0200

    Added cors package

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-const express = require('express');
-const app = express();
-const PORT = 3000;
+const express = require('express')
+const cors = require('cors')
 
-const fs = require('fs');
-const path = require('path');
-const GeoJsonGeometriesLookup = require("geojson-geometries-lookup");
+const app = express()
+app.use(cors())
 
-const lookupTable = require('./rep-lookup');
+const PORT = 3000
 
-app.use(express.static(path.join(__dirname, 'public')));
+const fs = require('fs')
+const path = require('path')
+const GeoJsonGeometriesLookup = require("geojson-geometries-lookup")
+
+const lookupTable = require('./rep-lookup')
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/rep', async (req, res) => {
-    const lat = parseFloat(req.query.lat);
-    const lon = parseFloat(req.query.lon);
+    const lat = parseFloat(req.query.lat)
+    const lon = parseFloat(req.query.lon)
 
     if (!lat || !lon) return
 
-    const f = fs.readFileSync(path.join(__dirname, 'public/circonscriptions-legislatives.json'), 'utf-8');
+    const f = fs.readFileSync(path.join(__dirname, 'public/circonscriptions-legislatives.json'), 'utf-8')
     const data = JSON.parse(f)
     
-    gl = new GeoJsonGeometriesLookup(data);
+    gl = new GeoJsonGeometriesLookup(data)
 
     const pos = {
         type: "Point",
@@ -35,9 +39,9 @@ app.use('/rep', async (req, res) => {
         return
     }
 
-    const repFile = fs.readFileSync(path.join(__dirname, `public/reps/${lookupTable[code]}.json`), 'utf-8');
+    const repFile = fs.readFileSync(path.join(__dirname, `public/reps/${lookupTable[code]}.json`), 'utf-8')
 
-    res.end(repFile);
+    res.end(repFile)
 
     /**
      * GENERATE INITIAL DATA
@@ -47,10 +51,10 @@ app.use('/rep', async (req, res) => {
     // const output = {}
 
     // for (let i = 500; i < lookupTable.length; i++) {
-    //     const repId = lookupTable[i];
+    //     const repId = lookupTable[i]
     //     console.log(repId)
 
-    //     const response = await fetch(`https://www.assemblee-nationale.fr/dyn/deputes/${repId}`);
+    //     const response = await fetch(`https://www.assemblee-nationale.fr/dyn/deputes/${repId}`)
     //     const body = await response.text()
 
     //     const depRegex = new RegExp(/(data-content=\"\/dyn\/carte\/departement\/)([A-Za-z0-9]{2,})/g)
@@ -76,7 +80,7 @@ app.use('/rep', async (req, res) => {
     //     output[`${rep.departement}-${rep.circo}`] = repId
     // }
 
-    // res.json(output);
+    // res.json(output)
 })
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
